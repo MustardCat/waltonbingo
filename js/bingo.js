a5e0e9340f5dae91877ba49676e0fbcbec7d6164
MustardCat <MustardCat@users.noreply.github.com>
2018-11-20 16:58:25 -0600

    Add toggle for optional list that can be added/removed from possible options.  Need to make it prettier but it'll do for now

--- js/bingo.js
+++ js/bingo.js
@@ -11,14 +11,12 @@ var bingoLetters =
 var initialList =
 [
   "Conference of Champions",
-  "\"It's Dave, Right?\"",
   "Truck Stop Conference",
   "Makes animal noise",
   "Drug Reference",
   "Grateful Dead",
   "Mentions his wife (Lori)",
   "Mentions his son (Luke)",
-  "Dave is a Coal Burner",
   "\"Throw it down, big man\"",
   "Complains about refs",
   "Compliments the refs",
@@ -27,13 +25,9 @@ var initialList =
   "Creates Player Nickname",
   "Nature Fact",
   "\"That's not a foul\"",
-  "Dave corrects Bill",
   "Mentions his playing days",
-  "Won't answer Dave's question",
   "Yoga/Meditation Reference",
   "(Top 5) Worst play ever",
-  "Creationism",
-  "\"Fossil fuel\"",
   "Calls player by nickname",
   "John Wooden",
   "Bill's mic is cut off while talking",
@@ -47,15 +41,25 @@ var initialList =
   "Complains about ESPN",
   "\"Midget chairs\"",
   "\"Another timeout?\"",
-  "Asks Dave if he's been to a location",
+  "Asks partner if he's been to a location",
   "\"Great Pass\" (bad shot)",
   "\"Climb the ladder\"",
-  "\"Why do you hate my grandchildren?\"",
   "Mentions his bike",
   "Plenty of time for a comeback",
   "___ is much better than media perception",
   "Says it's someone's birthday"
 ];
+var optionalList =
+[
+  "\"It's Dave, Right?\"",
+  "Dave is a Coal Burner",
+  "Dave corrects Bill",
+  "Won't answer Dave's question",
+  "Creationism",
+  "\"Fossil fuel\"",
+  "\"Why do you hate my grandchildren?\"",
+];
+
 var resizeTextCount = 25;
 
 var middlePos = parseInt(cardDimension/2);
@@ -165,6 +169,16 @@ function DeleteCard()
 function RandomizeCard()
 {
   bingoOptions = initialList.slice(0);
+
+  //Add Optional List to Bingo Options if checked
+  if (document.getElementById("chkBxDave").checked)
+  {
+    for (i = 0; i < optionalList.length; i++)
+    {
+      bingoOptions.push(optionalList[i]);
+    }
+  }
+
   RandomizeSeed();
   Random.shuffle(randomEngine, bingoOptions);
   DeleteCard();
@@ -182,6 +196,16 @@ function LoadSeed()
 
     DeleteCard();
     bingoOptions = initialList.slice(0);
+
+    //Add Optional List to Bingo Options if checked
+    if (document.getElementById("chkBxDave").checked)
+    {
+      for (i = 0; i < optionalList.length; i++)
+      {
+        bingoOptions.push(optionalList[i]);
+      }
+    }
+
     Random.shuffle(randomEngine, bingoOptions);
     CreateBingoCard();
   }
